c515c1a76c22483be82eba1affa172ff0f8a7452
Andres Campuzano <campuxano88@gmail.com>
2025-04-28 13:05:26 -0500

    add ranking icons and infection indicator

--- src/components/floating-windows-content/LeaderboardContent.tsx
+++ src/components/floating-windows-content/LeaderboardContent.tsx
@@ -16,33 +16,62 @@ export const LeaderboardContent = ({
         {data?.players?.map((item, index) => (
           <li
             key={item.username} // Assuming username is unique
-            className="flex flex-col border-black m-5 p-2 bg-[#cacaca]"
+            className={`relative flex flex-col border-black m-5 p-2 border-2 ${
+              index === 0
+                ? "bg-[#FFD700]"
+                : index === 1
+                  ? "bg-[#E5E5E5]"
+                  : index === 2
+                    ? "bg-[#b87333]"
+                    : "bg-gray-300"
+            }`}
           >
+            {index < 3 && (
+              <img
+                src={`images/${
+                  index === 0
+                    ? "coin_gold"
+                    : index === 1
+                      ? "coin_silver"
+                      : "coin_bronze"
+                }_x2.png`}
+                alt="rank coin"
+                className="w-10 absolute -top-4 -left-4"
+              />
+            )}
+
+            {item.isInfected && (
+              <img
+                src="images/spider.png"
+                alt="spider card"
+                className="w-8 absolute -top-4 -right-4"
+              />
+            )}
             <div className="flex flex-col items-center mb-2">
-              <p className="text-lg">#{index + 1}</p>
-              <p className="text-xl">{item.username}</p>
+              <p className="text-lg leading-none">#{index + 1}</p>
+              <p className="text-xl leading-none">{item.username}</p>
             </div>
             <div className="flex gap-1 items-center">
               <img
                 src="images/freecell.png"
-                alt="freecell level icon"
-                className="w-5 h-5"
+                alt="freecell level"
+                className="w-5"
               />
               <p className="ml-1">Level {item.level}</p>
             </div>
             <div className="flex gap-1 items-center">
               <img
                 src="images/joystick.png"
-                alt="joystick xp icon"
-                className="w-5 h-5"
+                alt="joystick xp"
+                className="w-5"
               />
               <p className="ml-1">XP {item.xp}</p>
             </div>
             <div className="flex gap-1 items-center">
               <img
-                src="images/coin_gold.png"
-                alt="coin gold icon"
-                className="w-6 h-6"
+                src="images/coin_gold_x2.png"
+                alt="gold coin"
+                className="w-6"
               />
               <p className="">Gold {item.gold}</p>
             </div>
