8900d2f4b0b4f754b53720edb0483b800f6045e1
Andres Campuzano <campuxano88@gmail.com>
2025-04-29 16:42:12 -0500

    refactor GameMarketContent to use GameMarketItem component for cleaner code

---
+++ src/components/GameMarketItem.tsx
@@ -0,0 +1,39 @@
+import { formatCurrency } from "../utils/numbers.ts";
+
+export const GameMarketItem = ({
+  type,
+  name,
+  description,
+  cost,
+}: {
+  type: string;
+  name: string;
+  description: string;
+  cost: number;
+}) => {
+  return (
+    <li
+      className={`relative flex flex-col border-black m-5 p-2 border-2 text-center ${
+        type === "poison_delay"
+          ? "bg-[#6199E3]"
+          : type === "poison_leveling"
+            ? "bg-[#D3984A]"
+            : type === "poison_recovery"
+              ? "bg-[#E66D31]"
+              : "bg-[#B6B5B5]"
+      }`}
+    >
+      <img
+        src={`images/${type}.png`}
+        alt={type}
+        className="w-7 absolute -top-7 left-1/2 transform -translate-x-1/2"
+      />
+      <p className="text-xl mx-4 leading-none my-3">{name}</p>
+      <p className="leading-4">{description}</p>
+      <p className="text-lg font-bold mt-auto mb-2 pt-2">
+        <img src="images/coin_gold_x2.png" alt="gold" className="w-6 inline" />
+        {formatCurrency(cost)}
+      </p>
+    </li>
+  );
+};
--- src/components/floating-windows-content/GameMarketContent.tsx
+++ src/components/floating-windows-content/GameMarketContent.tsx
@@ -1,5 +1,5 @@
 import { ErrorMessage } from "../common/ErrorMessage";
-import { formatCurrency } from "../../utils/numbers.ts";
+import { GameMarketItem } from "../GameMarketItem.tsx";
 
 export const GameMarketContent = ({
   data,
@@ -36,33 +36,13 @@ export const GameMarketContent = ({
       <section>
         <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
           {data?.items?.map((item) => (
-            <li
-              className={`relative flex flex-col border-black m-5 p-2 border-2 text-center ${
-                item.type === "poison_delay"
-                  ? "bg-[#6199E3]"
-                  : item.type === "poison_leveling"
-                    ? "bg-[#D3984A]"
-                    : item.type === "poison_recovery"
-                      ? "bg-[#E66D31]"
-                      : "bg-[#B6B5B5]"
-              }`}
-            >
-              <img
-                src={`images/${item.type}.png`}
-                alt={item.type}
-                className="w-7 absolute -top-7 left-1/2 transform -translate-x-1/2"
-              />
-              <p className="text-xl mx-4 leading-none my-3">{item.name}</p>
-              <p className="leading-4">{item.description}</p>
-              <p className="text-lg font-bold mt-auto mb-2 pt-2">
-                <img
-                  src="images/coin_gold_x2.png"
-                  alt="gold"
-                  className="w-6 inline"
-                />
-                {formatCurrency(item.cost)}
-              </p>
-            </li>
+            <GameMarketItem
+              key={item.type}
+              type={item.type}
+              name={item.name}
+              description={item.description}
+              cost={item.cost}
+            />
           ))}
         </ul>
       </section>
