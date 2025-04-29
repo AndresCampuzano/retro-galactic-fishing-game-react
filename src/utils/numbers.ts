bc4345322fbb1172d69311d8880242932b14c227
Andres Campuzano <campuxano88@gmail.com>
2025-04-29 16:38:44 -0500

    docs

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,5 +1,7 @@
 /**
  * Formats a number to a currency format.
+ * @example
+ * formatCurrency(1234567) // returns "1,234,567"
  */
 export function formatCurrency(value: number): string {
   return value.toLocaleString("en-US", {
